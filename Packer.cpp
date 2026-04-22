def24baf399fcbf1d8348ea8bb7fe873f7195b7a
wsw1055 <63052830+wsw1055@users.noreply.github.com>
2026-04-22 16:18:56 +0800

    Add files via upload

---
+++ Packer.cpp
@@ -0,0 +1,18 @@
+﻿// Packer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Packer.h"
+
+int main(int argc, char* argv[])
+{
+	//usage:packer <input.exe>  <output.exe>
+	if (argc < 3) {
+		printf("usage:packer <input.exe>  <output.exe>\n");
+		return 0;
+	}
+
+	Packer packer(argv[1], argv[2]);
+	packer.pack();
+	return 0;
+}
